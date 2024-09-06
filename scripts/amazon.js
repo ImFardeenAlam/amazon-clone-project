6817d514edf2b652eaeee083d0081614bab2ee66
fardeenalammaruf <dggaming1969@outlook.com>
2024-09-07 00:23:48 +0600

    fixed the checkout page cartQuantity and took action to reduce the code

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart,  calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -58,15 +58,12 @@ products.forEach((product) => {
   `;
 });
 
-
 function updateCartQuantity(){
-  let cartQuantity = 0;
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
+ const cartQuantity = calculateCartQuantity();
+
+ document.querySelector('.js-cart-quantity')
+  .innerHTML = cartQuantity;
+};
 
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { calculateCartQuantity, cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -97,7 +97,6 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
-console.log(cartSummaryHTML);
 
 document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
@@ -109,5 +108,13 @@ document.querySelectorAll('.js-delete-link')
         `.js-cart-item-container-${productId}`
       );
       container.remove();
+      updateCartQuantity();
     });
   });
+  //checkout page quantity at top
+  function updateCartQuantity(){
+   const cartQuantity = calculateCartQuantity();
+    document.querySelector('.js-return-to-home-link')
+    .innerHTML = cartQuantity;
+  };
+  updateCartQuantity();
