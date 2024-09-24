4eae995dd5bac2545afb505a36d4fff36693be56
fardeenalammaruf <dggaming1969@outlook.com>
2024-09-24 11:45:14 +0600

    Create automated tests for orderSummary

--- scripts/Checkout/orderSummary.js
+++ scripts/Checkout/orderSummary.js
@@ -23,7 +23,9 @@ export function renderOrderSummary(){
     const dateString = calculateDeliveryDate(deliveryOption);
     
     cartSummaryHTML += `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container 
+      js-cart-item-container 
+      js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: ${dateString}
       </div>
@@ -39,7 +41,8 @@ export function renderOrderSummary(){
           <div class="product-price">
             ${formatCurrency(matchingProduct.priceCents)}
           </div>
-          <div class="product-quantity">
+          <div class="product-quantity 
+          js-product-quantity-${matchingProduct.id}">
             <span>
               Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
@@ -48,7 +51,7 @@ export function renderOrderSummary(){
             </span>
             <input class="quantity-input js-quantity-input-${matchingProduct.id}">
             <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id = ${matchingProduct.id}>Save</span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id = ${matchingProduct.id}>
+            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id = ${matchingProduct.id}>
               Delete
             </span>
           </div>
@@ -117,12 +120,15 @@ export function renderOrderSummary(){
         renderPaymentSummary();
       });
     });
-    //checkout page quantity at top
-    function updateCartQuantity(){
-    const cartQuantity = calculateCartQuantity();
-      document.querySelector('.js-return-to-home-link')
-      .innerHTML = cartQuantity;
-    };
+//checkout page quantity at top
+function updateCartQuantity(){
+  const cartQuantity = calculateCartQuantity();
+  const returnHomeLink = document.querySelector('.js-return-to-home-link');
+  if (returnHomeLink) {
+    returnHomeLink.innerHTML = cartQuantity;
+  }
+};
+
     updateCartQuantity();
 
   document.querySelectorAll('.js-update-quantity-link')
